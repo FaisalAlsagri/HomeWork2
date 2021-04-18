8bde4f7d869442fff9cd484b37a5c18e2c84977b
Faisal Abdulaziz <faisal.alsagri.1@gmail.com>
2021-04-19 02:22:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuotBuilder obj = new QuotBuilder();
+            System.Console.WriteLine(obj.beginQuot()
+                .setContent("Hello World!")
+                    .endQuot()
+                        .getQuot());
+        }
+        class QuotBuilder {
+            private string quot;
+            public QuotBuilder()
+            {
+                this.quot = "";
+            }
+            public QuotBuilder beginQuot()
+            {
+                this.quot += "\"";
+                return this;
+            }
+            public QuotBuilder setContent(string value)
+            {
+                this.quot += value;
+                return this;
+            }
+            public QuotBuilder endQuot()
+            {
+                this.quot += "\"";
+                return this;
+            }
+            public string getQuot()
+            {
+                return this.quot;
+            }
+        }
+    }
+}
